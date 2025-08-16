ca105122c15c79cd40fc64a222b8c6241d9820df
Tahcin Ul Karim Mycin <150533238+mmycin@users.noreply.github.com>
2025-08-16 15:13:25 +0600

    Prisma setup, apu route setup, component structure setup

---
+++ src/components/General/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button: React.FC = () => {
+  return (
+    <div>
+        <h1>Hello Button</h1>
+    </div>
+  );
+};
+
+export default Button;
---
+++ src/components/pages/Home/Starter.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Starter: React.FC = () => {
+  return (
+    <div>
+        <h1>Hello Starter</h1>
+    </div>
+  );
+};
+
+export default Starter;
